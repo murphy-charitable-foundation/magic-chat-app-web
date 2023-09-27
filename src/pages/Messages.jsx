c14eb8c0d7789ea3232f4f72bacf5dbdd4aa5636
charlie <charlie.a.fischer@gmail.com>
2023-09-26 17:50:38 -0700

    started building out chats paage more - has a mock chats response and a search to filter by name

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -1,23 +1,96 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
+import { Avatar, Box, Stack, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+
+const chatsRes = [
+  {
+    name: "Festus",
+    imgSrc: "./images/festus.jpg",
+    lastMessage: "Hey, How are you?",
+    timestamp: "12:01",
+  },
+  {
+    name: "Charlie",
+    imgSrc: "./images/example-1.jpeg",
+    lastMessage: "Hey, How are you?",
+    timestamp: "11:10",
+  },
+];
 
 const Messages = () => {
+  const [chats, setChats] = useState(chatsRes);
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchTextChange = (event) => {
+    const newText = event.target.value;
+    setSearchText(newText);
+    const filteredChats = chatsRes.filter((chat) =>
+      chat.name.toLowerCase().includes(newText.toLowerCase())
+    );
+    setChats(filteredChats);
+  };
+
   return (
     <Box>
-      <Typography variant="h5">
-        Your Interaction
-        <Typography variant="subtitle2" ml={1} component="small">
-          Get in touch with someone
-        </Typography>{" "}
-      </Typography>
+      <Typography variant="h1">Chats</Typography>
+      <TextField
+        id="outlined-basic"
+        label="Search"
+        variant="outlined"
+        fullWidth
+        value={searchText}
+        onChange={handleSearchTextChange}
+      />
       <Typography variant="paragragh" paddingX={2} marginX={2}>
         <hr />
       </Typography>
-      <Typography variant="paragraph" component="p">
-        ...
-      </Typography>
-
-      <p></p>
+      <Stack
+        direction="column"
+        justifyContent="flex-start"
+        alignItems="stretch"
+        spacing={2}
+      >
+        {chats.map((chat, i) => (
+          <Box key={i}>
+            <Stack
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="center"
+              spacing={2}
+            >
+              <Avatar
+                alt={chat.name}
+                sx={{ alignContent: "end" }}
+                src={chat.imgSrc}
+              />
+              <Stack
+                direction="column"
+                justifyContent="flex-start"
+                alignItems="stretch"
+                spacing={2}
+              >
+                <Typography variant="h5" sx={{ fontWeight: "bold", mt: 0 }}>
+                  {chat.name}
+                </Typography>
+                <Typography variant="paragragh">{chat.lastMessage}</Typography>
+              </Stack>
+              <Typography
+                variant="paragraph"
+                sx={{
+                  fontStyle: "italic",
+                  alignSelf: "flex-start",
+                  flex: 1,
+                  textAlign: "end",
+                }}
+              >
+                {chat.timestamp}
+              </Typography>
+            </Stack>
+            <Typography variant="paragragh" paddingX={2} marginX={2}>
+              <hr />
+            </Typography>
+          </Box>
+        ))}
+      </Stack>
     </Box>
   );
 };
