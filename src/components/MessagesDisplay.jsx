e281d8703b1c973fea0fda44e29406af80748d9f
charlie <charlie.a.fischer@gmail.com>
2023-11-28 11:39:29 -0800

    reformated date

--- src/components/MessagesDisplay.jsx
+++ src/components/MessagesDisplay.jsx
@@ -1,15 +1,29 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
 import Message from "./Message";
 
 export default function MessagesDisplay({ chat = [] }) {
   const orderedChat = chat.sort((a, b) => a.time.seconds - b.time.seconds);
-  const groupedMessages = Object.groupBy(orderedChat, msg => {
-    const date = new Date(
-      msg.time.seconds * 1000 + msg.time.nanoseconds / 1e6
-    );
-    return date.toDateString();
-  })
+
+  const formatDate = (date) => {
+    const today = new Date();
+    const messageDate = new Date(date.seconds * 1000 + date.nanoseconds / 1e6);
+
+    if (
+      messageDate.getDate() === today.getDate() &&
+      messageDate.getMonth() === today.getMonth() &&
+      messageDate.getFullYear() === today.getFullYear()
+    ) {
+      return "Today";
+    }
+    const options = { weekday: "short", month: "short", day: "numeric" };
+    return messageDate.toLocaleDateString("en-US", options);
+  };
+
+  const groupedMessages = Object.groupBy(orderedChat, (msg) => {
+    return formatDate(msg.time);
+  });
+
   return (
     <div>
       <Box maxHeight="40vh" overflow="scroll" justifyContent="end">
@@ -22,8 +36,19 @@ export default function MessagesDisplay({ chat = [] }) {
         >
           {Object.entries(groupedMessages).map(([date, messages]) => (
             <React.Fragment key={date}>
-              <Box>
-                <strong>{date}</strong>
+              <Box
+                m="0 0 24px 0"
+                sx={{
+                  alignSelf: "center",
+                  background: "#DDDDE9",
+                  borderRadius: "8px",
+                  width: "fit-content",
+                  padding: "4px 24px",
+                }}
+              >
+                <Typography color="#3C3C43" fontSize="12px">
+                  {date}
+                </Typography>
               </Box>
               {messages.map((message) => (
                 <Message messageData={message} key={message.id} />
