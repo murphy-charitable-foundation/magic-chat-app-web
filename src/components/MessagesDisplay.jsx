b42815d86750cada5b1a6dad27a6ffd73ab20eb2
charlie <charlie.a.fischer@gmail.com>
2023-11-28 11:24:25 -0800

    refactor

--- src/components/MessagesDisplay.jsx
+++ src/components/MessagesDisplay.jsx
@@ -4,24 +4,12 @@ import Message from "./Message";
 
 export default function MessagesDisplay({ chat = [] }) {
   const orderedChat = chat.sort((a, b) => a.time.seconds - b.time.seconds);
-  // const groupedMessages = {};
   const groupedMessages = Object.groupBy(orderedChat, msg => {
     const date = new Date(
       msg.time.seconds * 1000 + msg.time.nanoseconds / 1e6
     );
     return date.toDateString();
   })
-  // orderedChat.forEach((message) => {
-  //   console.log(message);
-  //   const date = new Date(
-  //     message.time.seconds * 1000 + message.time.nanoseconds / 1e6
-  //   );
-  //   const dateKey = date.toDateString();
-  //   if (!groupedMessages[dateKey]) {
-  //     groupedMessages[dateKey] = [];
-  //   }
-  //   groupedMessages[dateKey].push(message);
-  // });
   return (
     <div>
       <Box maxHeight="40vh" overflow="scroll" justifyContent="end">
