fae79f589b235f3f666a6675236df4c68437ce4d
charlie <charlie.a.fischer@gmail.com>
2023-11-28 12:02:06 -0800

    icons added, no functionality to chat immages/voice

--- src/components/NewMessage.jsx
+++ src/components/NewMessage.jsx
@@ -1,22 +1,37 @@
 import { Button, Stack, TextField } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
+import KeyboardVoiceOutlinedIcon from '@mui/icons-material/KeyboardVoiceOutlined';
 import React from "react";
 
 export default function NewMessage({ newMessage, sendMessage, setNewMessage }) {
   return (
-    <Stack direction="row" sx={{ alignItems: "center", marginTop: "12px" }}>
-      <form onSubmit={sendMessage}>
-        <TextField
-          placeholder="Your Message..."
-          value={newMessage}
-          onChange={(e) => {
-            setNewMessage(e.target.value);
-          }}
-          id="new-message"
-          sx={{
-            flex: 1,
-          }}
-        ></TextField>
-        <Button type="submit">Send</Button>
+    <Stack direction="row" alignItems="center" marginTop="12px">
+      <AddIcon htmlColor="#007AFF" />
+      <form onSubmit={sendMessage} style={{"width": "100%"}}>
+        <Stack direction="row" alignItems="center">
+          <TextField
+            placeholder="Your Message..."
+            value={newMessage}
+            onChange={(e) => {
+              setNewMessage(e.target.value);
+            }}
+            id="new-message"
+            sx={{
+              flex: 1,
+            }}
+          ></TextField>
+          {newMessage ? (
+            <Button type="submit">Send</Button>
+          ) : (
+            <Stack direction="row">
+              <CameraAltOutlinedIcon htmlColor="#007AFF" />
+              <KeyboardVoiceOutlinedIcon htmlColor="#007AFF" />
+            </Stack>
+          )
+          }
+        </Stack>
+        
       </form>
     </Stack>
   );
