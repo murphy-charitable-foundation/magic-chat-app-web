35b25efcd0d4abc4d6a3c5a715774bd72b6255c7
Rez G <rezg@Rezs-MBP.ht.home>
2024-03-04 07:07:28 -0800

    make UI more consistent

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import {
+  Typography,
+} from "@mui/material";
+
 
 const ResetPassword = () => {
   const auth=getAuth () ;
@@ -32,17 +36,19 @@ const ResetPassword = () => {
   );
   return <div style={{textAlign:'center',paddingTop:'50px'}}>
   <div style={{maxWidth:'400px',margin:'auto'}}>
-      <h1 style={{marginBottom:'50px'}}>Reset your password</h1>
+      <Typography component="h1" variant="h5" >         
+        Reset your password
+      </Typography>
       <img src="./logo.png" alt="Logo" style={{width:'100px',height:'auto',marginBottom:'20px'}}/>
       <div style={{marginBottom:'20px'}}>
-          <input type="email" id="email" placeholder="Write your e-mail registered" style={{padding:'10px','width':'100%'}}/>
+          <input type="email" id="email" placeholder="Write your e-mail registered" style={{padding:'10px','width':'100%', marginBottom: '60px'}}/>
       </div>
       <button onClick={resetPassword} style={{padding:'10px',width:'100%',backgroundColor:'#48801c',color:'white',borderRadius:'20px'}}>Reset</button>
   </div>
   {showModal && (
         <div style={{position: 'fixed',zIndex: '1',paddingTop: '100px',left: '0',top: '0',width: '100%',height: '100%',overflow: 'auto',backgroundColor: 'rgb(0,0,0)',backgroundColor: 'rgba(0,0,0,0.4)'}}>
           <div style={{backgroundColor: '#fefefe',margin: 'auto',padding: '20px',border: '1px solid #888',width: '80%',boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',animationName: 'animatetop',animationDuration: '0.4s'}}>
-            <h2 style={{color: '#023268'}}>Check your email</h2>
+            <h3 style={{color: '#023268'}}>Check your email</h3>
             <p>Please check your email inbox and spam folder for a verification email to reset your password.</p>
             <button onClick={() => setShowModal(false)} style={{ backgroundColor: '#48801c', color: 'white', padding: '10px 20px', margin: '10px 0', borderRadius:'20px'}}>Understood</button>
           </div>
