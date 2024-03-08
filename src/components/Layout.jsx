69b7b3b96b53be75f0c0b2bec1b50fb5179954fd
GU <90733954+jg6604@users.noreply.github.com>
2024-03-08 01:12:28 -0500

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,16 @@
 import React from "react";
-import Header from "./Header";
 import { Outlet } from "react-router";
-import Footer from "./Footer";
 import { Container, Paper } from "@mui/material";
 
 const Layout = () => {
   return (
     <>
-      <Header />
       <Container sx={{ maxWidth: "90%", marginTop: 3 }}>
         <Paper sx={{ minHeight: 300, padding: 3 }}>
           <Outlet />
         </Paper>
       </Container>
 
-      <Footer />
     </>
   );
 };
