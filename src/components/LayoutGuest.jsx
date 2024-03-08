e5e5d93fbceaba5d6f8859e9aaee3804825ede21
GU <90733954+jg6604@users.noreply.github.com>
2024-03-08 01:13:06 -0500

    Update LayoutGuest.jsx

--- src/components/LayoutGuest.jsx
+++ src/components/LayoutGuest.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import GuestHeader from "./HeaderGuest";
+
 import { Outlet } from "react-router";
-import Footer from "./Footer";
+
 import { Container, Paper } from "@mui/material";
 
 const LayoutGuest = () => {
   return (
     <>
-      <GuestHeader />
+     
       <Container sx={{ maxWidth: "90%", marginTop: 3 }}>
         <Paper sx={{ minHeight: 300, padding: 3 }}>
           <Outlet />
         </Paper>
       </Container>
-      <Footer />
+     
     </>
   );
 };
